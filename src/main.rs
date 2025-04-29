283e81685a5f6b6cb2ccdd87b83a2a08891d12d1
Francesco Pompo <pfrankw@users.noreply.github.com>
2025-04-30 00:35:58 +0200

    Added main

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use hackshell::{Event, Hackshell, InputProvider};
+use readline::Readline;
+use tokio::io::{self, AsyncRead, Stdin, stdin};
+
+struct Context {}
+struct MyInputProvider<R> {
+    rl: Readline<R>,
+}
+
+impl MyInputProvider<Stdin> {
+    pub async fn new() -> Self {
+        Self {
+            rl: Readline::new(stdin(), "> ", None).await,
+        }
+    }
+}
+
+impl<R: AsyncRead + Unpin> InputProvider for MyInputProvider<R> {
+    async fn read_line(&self) -> io::Result<Event> {
+        Readline::<R>::enable_raw_mode()?;
+        let r = Ok(Event::Line(self.rl.run().await?));
+        Readline::<R>::disable_raw_mode()?;
+        r
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let ctx = Context {};
+    let myip = MyInputProvider::new().await;
+    let s = Hackshell::new(ctx, myip, "> ", None).await;
+
+    loop {
+        s.run().await;
+    }
+}
