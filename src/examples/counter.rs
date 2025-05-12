4307b9c67fadecf0c507fe237454ae02129edcab
Francesco Pompo <pfrankw@users.noreply.github.com>
2025-05-12 12:02:07 +0200

    Added counter example

---
+++ src/examples/counter.rs
@@ -0,0 +1,52 @@
+use hackshell::{Command, Hackshell, error::MapErrToString};
+use tokio::sync::RwLock;
+
+struct MyContext {}
+
+struct Counter {
+    counter: RwLock<u64>,
+}
+
+#[async_trait::async_trait]
+impl Command<MyContext> for Counter {
+    fn commands(&self) -> &'static [&'static str] {
+        &["counter"]
+    }
+
+    fn help(&self) -> &'static str {
+        "This is a non-default command installed by the Hackshell consumer. It simply increments an internal counter"
+    }
+
+    async fn run(
+        &self,
+        _s: &Hackshell<MyContext>,
+        _cmd: &[String],
+        _ctx: &MyContext,
+    ) -> Result<(), String> {
+        let mut counter = self.counter.write().await;
+
+        *counter += 1;
+
+        println!("The counter is now: {}\r", counter);
+
+        Ok(())
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), String> {
+    let ctx = MyContext {};
+
+    let s = Hackshell::new(ctx, "hackshell> ", None)
+        .await
+        .to_estring()?;
+
+    s.add_command(Counter {
+        counter: RwLock::new(0),
+    })
+    .await;
+
+    loop {
+        s.run().await.to_estring()?;
+    }
+}
